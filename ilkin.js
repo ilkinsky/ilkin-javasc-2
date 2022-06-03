1e7ae349b0c50e17693961a284fb12e98a5e0e53
ilkinsky <ilkinehmedzade35@gmail.com>
2022-06-03 10:19:22 +0400

    finish

---
+++ ilkin.js
@@ -0,0 +1,16 @@
+let a=6
+let b=18
+let c=21
+
+console.log(a == b)
+console.log(c == b)
+console.log(a < b)
+console.log(b < c)
+
+if (b < c) {
+    console.log("c b  den boyukdur") 
+}
+
+if (a < c+b){
+    console.log("c ile b nin cemi,a dan boyukdur")
+}
